8b362870bf3ad50eaacfe110a539e5cbc9c8e6c6
Dg <work@dgbragas.com>
2025-01-24 18:20:31 -0300

    feat: create ds-library content type

---
+++ src/api/ds-library/controllers/ds-library.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ds-library controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ds-library.ds-library');
---
+++ src/api/ds-library/routes/ds-library.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ds-library router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ds-library.ds-library');
