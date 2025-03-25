4e6c61e93be92fc815b7a302db4ee3a1131f26f1
Dg <work@dgbragas.com>
2025-03-25 01:16:17 -0300

    test: remove cloudinaryProvider import

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,17 @@
-const cloudinaryRaw = require('@strapi/provider-upload-cloudinary');
-const cloudinaryProvider = cloudinaryRaw.default || cloudinaryRaw;
-
-module.exports = ({ env }) => {
-  console.log('☁️ Cloudinary Provider Loaded:', typeof cloudinaryProvider);
-
-  return {
-    upload: {
-      config: {
-        provider: cloudinaryProvider,
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-  };
-};
+  },
+});
