df1638254897be2faa61c9204ff3e3854e5def0b
Dg <work@dgbragas.com>
2025-03-25 00:20:22 -0300

    chore: debug cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,21 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+module.exports = ({ env }) => {
+  console.log('Cloudinary Ready!');
+
+  return {
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
       },
     },
-  },
-});
+  };
+};
